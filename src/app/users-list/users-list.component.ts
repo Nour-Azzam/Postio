c721b390b5aa6f1b9a592e4693093aba07851e84
Nour Azzam <azzamnour2002@gmail.com>
2023-03-19 14:17:29 +0300

    add search user functionality

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { tap } from 'rxjs';
 import { CacheDataService } from 'src/assets/cacheData.service';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
 })
 export class UsersListComponent implements OnInit {
-
   usersList$ = this.cacheService.users$;
+  @ViewChild('userSearch') userSearch?: ElementRef;
 
-  constructor(private cacheService: CacheDataService) { }
+  constructor(
+    private cdref: ChangeDetectorRef,
+    private cacheService: CacheDataService,
+    ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSearchUser() {
+    this.usersList$ = this.cacheService.searchUser(
+      this.userSearch?.nativeElement.value
+    )
   }
-
 }
